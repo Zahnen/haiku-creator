eb47180e3da3b7a8c2869bb9c04733860c466ae1
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-12 11:35:03 -0800

    show good fail on first line spec
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- __tests__/line.test.js
+++ __tests__/line.test.js
@@ -5,24 +5,30 @@ describe('Line', () => {
   let line;
 
   beforeEach(() => {
-    line = new Line("I hope this test fails");
+    line = new Line("I hope this test fails", [], 0);
   });
 
-  test('should correctly split string into words', () => {
-    expect(line.split("")).toMatch("I", "hope", "this", "test", "fails");
+  test('should correctly create a line object with string, words, and syllables', () => {
+    expect(line.string).toMatch();
+    expect(line.words).toBeUndefined([]);
+    expect(line.syllables).toEqual(0);
   });
 
-  test('should correctly count vowels within each word', () => {
-    expect("I").toEqual(1);
-  });
+  // test('should correctly split string into words', () => {
+  //   expect(line.split("")).toMatch("I", "hope", "this", "test", "fails");
+  // });
 
-  test('should correctly subtract one vowel in words that contain diphthongs', () => {
-    expect("fails").toEqual(1);
-  });
+  // test('should correctly count vowels within each word', () => {
+  //   expect("I").toEqual(1);
+  // });
 
-  test('should correctly subtract one vowel from the vowel total', () => {
-    expect("hope").toEqual(1);
-  });
+  // test('should correctly subtract one vowel in words that contain diphthongs', () => {
+  //   expect("fails").toEqual(1);
+  // });
+
+  // test('should correctly subtract one vowel from the vowel total', () => {
+  //   expect("hope").toEqual(1);
+  // });
 
 });
   
