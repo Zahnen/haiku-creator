7856898bb4124430690b4cd3ae74af86fc93370e
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-12 16:00:10 -0800

    show that checkDipth is failing
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- __tests__/line.test.js
+++ __tests__/line.test.js
@@ -5,11 +5,12 @@ describe('Line', () => {
   let line;
 
   beforeEach(() => {
-    line = new Line("I hope this test fails", [], 0);
+    line = new Line("I hope this test fails",0, [], 0);
   });
 
   test('should correctly create a line object with string, words, and syllables', () => {
     expect(line.string).toEqual("I hope this test fails");
+    expect(line.vowelCount).toEqual(0);
     expect(line.words).toEqual([]);
     expect(line.syllables).toEqual(0);
   });
@@ -22,9 +23,9 @@ describe('Line', () => {
     expect(line.countVowels()).toEqual(7);
   });
 
-  // test('should correctly subtract one vowel in words that contain diphthongs', () => {
-  //   expect("fails").toEqual(1);
-  // });
+  test('should correctly subtract one vowel in words that contain diphthongs', () => {
+    expect(line.checkDipth()).toEqual(-2);
+  });
 
   // test('should correctly subtract one vowel from the vowel total', () => {
   //   expect("hope").toEqual(1);
--- src/line.js
+++ src/line.js
@@ -1,24 +1,44 @@
 export default class Line {
-  constructor(string, words, syllables) {
+  constructor(string, vowelCount, words, syllables) {
     this.string = string;
+    this.vowelCount = 0
     this.words = [];
     this.syllables = 0;
   }
-
   
   countVowels() { 
-    let vowelCount = 0;
     let countString = (this.string).toLowerCase();
     for (let i = 0; i<= countString.length-1; i++) {
-      if (countString.charAt(i) == "a" || countString.charAt(i) == "e" || countString.charAt(i) == "i" || countString.charAt(i) == "o" || countString.charAt(i) == "u") {
-      vowelCount += 1;
+      if (countString.charAt(i) == "a" || countString.charAt(i) == "e" || countString.charAt(i) == "i" || countString.charAt(i) == "o" || countString.charAt(i) == "u" || countString.charAt(i) == "y") {
+      this.vowelCount += 1;
       }
     }
-    return vowelCount;
+    return this.vowelCount;
   }
-    
-      splitWord() {
-        let words = (this.string).split(" ");
-        return words;
+
+  checkDipth() {
+    let words = (this.string).split(" ");
+    let vowelNum = this.vowelCount;
+    const globalRegex = /["ai", "oo"]/g
+    words.forEach (function(element) {
+      if (globalRegex.test(element)) {
+      vowelNum -= 1;
       }
+    });  
+    return vowelNum;
+  }
+
+  splitWord() {
+    let words = (this.string).split(" ");
+    return words;
+  }
 };
+
+
+// let vowelAmount = this.vowelCount
+  //   let countString = (this.string).toLowerCase();
+  //   if (countString.includes("au" || "oo" || "oi" || "ou" || "ey" || "ai" || "ea" || "oy" || "ay" || "ie" || "ei")) {
+  //     vowelAmount -= 1;
+  //   }
+  //     return vowelAmount; 
+  // }
